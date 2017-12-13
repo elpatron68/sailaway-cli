8eb6ea2245faca7c5ed913b6d75ef382bddc18e3
markus <markus@medisoftware.de>
2017-12-13 16:08:44 +0100

    removed blanks

--- src/main.py
+++ src/main.py
@@ -1,19 +1,19 @@
+import sys
 import api
 import gpsd
-import sys
 
-def usage ():
+def usage():
     pass
 
-def main ():
-    argvn = len (sys.argv)
+def main():
+    argvn = len(sys.argv)
 
-    print (sys.argv)
+    print(sys.argv)
 
     # No args, usage
     if argvn < 2:
-        usage ()
-        sys.exit ()
+        usage()
+        sys.exit()
 
     # login
     elif argvn > 1 and sys.argv[1] == 'login':
@@ -23,37 +23,37 @@ def main ():
         if argvn > 2:
             email = sys.argv[2]
         else:
-            email = input ('Email: ')
+            email = input('Email: ')
         if argvn > 3:
             password = sys.argv[3]
         else:
-            password = input ('Password: ')
+            password = input('Password: ')
 
-        sw = api.Sailaway ()
-        sw.login (email, password)
+        sw = api.Sailaway()
+        sw.login(email, password)
 
     # list-boats
     elif argvn > 1 and sys.argv[1] == 'list-boats':
-        sw = api.Sailaway ()
-        sw.login ()
-        for boat in sw.getUserBoats ()['userboats']:
-            print (boat['ubtnr'], '\t', boat['ubtname'], '\t', boat['prdtitle'])
+        sw = api.Sailaway()
+        sw.login()
+        for boat in sw.getUserBoats()['userboats']:
+            print(boat['ubtnr'], '\t', boat['ubtname'], '\t', boat['prdtitle'])
 
-        
+
     # gps-serve
     elif argvn > 2 and sys.argv[1] == 'gps-serve':
         boat = sys.argv[2]
         port = 2947
 
         if argvn > 3:
-            port = int (sys.argv[3])
+            port = int(sys.argv[3])
 
-        sw = api.Sailaway ()
-        sw.login ()
+        sw = api.Sailaway()
+        sw.login()
 
-        gdaemon = gpsd.GPSD ()
-        gdaemon.serve (sw, boat, port)
+        gdaemon = gpsd.GPSD()
+        gdaemon.serve(sw, boat, port)
 
 
 if __name__ == "__main__":
-    main ()
+    main()
